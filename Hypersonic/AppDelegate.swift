7a1577a24a74b99c9d159c51a412693a8bbadcb3
南知 <deer_hope@live.cn>
2024-04-08 07:23:42 +0800

    Add main app

---
+++ Hypersonic/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Hypersonic
+//
+//  Created by 南朋友 on 2024/4/8.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Hypersonic/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hypersonic
+//
+//  Created by 南朋友 on 2024/4/8.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
