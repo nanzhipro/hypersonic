7b0746a31681f79fe30a127d72ab124cb241f446
南知 <deer_hope@live.cn>
2024-04-15 08:43:42 +0800

    feat：HelperManager基本框架

---
+++ Hypersonic/HerlperManager.swift
@@ -0,0 +1,27 @@
+//
+//  HerlperManager.swift
+//  Hypersonic
+//
+//  Created by 南朋友 on 2024/4/15.
+//
+
+import Foundation
+import ServiceManagement
+
+public class HelperManager {
+    public init() {
+        
+    }
+    
+    public func installHelper()  {
+        
+    }
+    
+    public func removeHelper() {
+        
+    }
+    
+//    public func getHelperStatus() -> SMAppService.Status {
+//        
+//    }
+}
